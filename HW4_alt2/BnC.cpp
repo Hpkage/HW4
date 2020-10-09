985b12e2dccc36a519f112d47c7f03c8fa097c28
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-10-09 03:10:07 -0800

    Gen number

--- HW4_alt2/BnC.cpp
+++ HW4_alt2/BnC.cpp
@@ -9,3 +9,9 @@
 
 using namespace std;
 
+string generate_secret_number() {
+    // Generates a 4-digit secret number (between 1000 and 9999)
+    srand(time(0));
+    int secret_number = rand() % 9000 + 1000;
+    return to_string(secret_number);
+}
