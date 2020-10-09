57d19f7d5954d164f7419aff4e705c0b92541e14
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-10-09 03:10:43 -0800

    User input

--- HW4_alt2/BnC.cpp
+++ HW4_alt2/BnC.cpp
@@ -15,3 +15,15 @@ string generate_secret_number() {
     int secret_number = rand() % 9000 + 1000;
     return to_string(secret_number);
 }
+
+string get_user_input() {
+    int guess = 0;
+    cin >> guess;
+
+    // Checks if guess is a 4-digit number (between 1000 and 9999)
+    while (guess < 1000 || guess > 9999) {
+        cout << "Invalid input!" << endl;
+        cin >> guess;
+    }
+    return to_string(guess);
+}
