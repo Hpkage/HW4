7c9346c7d6a258ba00d93f91eddbd3f1ba603c3f
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-10-09 01:11:21 -0800

    Options

--- HW4_alt1/crud.cpp
+++ HW4_alt1/crud.cpp
@@ -5,8 +5,13 @@
 
 using namespace std;
 
-int main() {
-
-
-	return 0;
+void add() {
+    cout << " a. Add a book" << endl;
+    cout << " b. Remove a book" << endl;
+    cout << " c. Empty database" << endl;
+    cout << " d. show number of books" << endl;
+    cout << " e. show all of books" << endl;
+    cout << " f . quit" << endl;
+           
+	return;
 }
