49ed656efba4860d93cc870895fc074b81b156e4
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-10-09 01:14:08 -0800

    Start CRUD

--- HW4_alt1/crud.cpp
+++ HW4_alt1/crud.cpp
@@ -5,13 +5,15 @@
 
 using namespace std;
 
-void add() {
-    cout << " a. Add a book" << endl;
-    cout << " b. Remove a book" << endl;
-    cout << " c. Empty database" << endl;
-    cout << " d. show number of books" << endl;
-    cout << " e. show all of books" << endl;
-    cout << " f . quit" << endl;
-           
-	return;
+void crud() {
+    char choice;
+    string bookname;
+    string removebook;
+    int countnum;
+    string authorname;
+    string fullbook;
+    vector<string> bookarray;
+    cin >> choice;
+    
+    return;
 }
